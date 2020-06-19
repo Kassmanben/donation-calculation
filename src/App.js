73af59e095bdeee85ed217d71092233ac8050e1f
Kassmanben <kassmanben@gmail.com>
2020-06-18 18:14:36 -0700

    update to address safari formatting

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ class App extends React.Component {
   }
 
   numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    return x.toLocaleString();
   }
 
   calculateAmount() {
